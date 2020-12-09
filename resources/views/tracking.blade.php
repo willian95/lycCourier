8becd818cfac7a2d33daaa8062ed78447788f03c
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-12-09 12:17:47 -0400

    cambios

--- resources/views/tracking.blade.php
+++ resources/views/tracking.blade.php
@@ -553,10 +553,12 @@
 
         <section class="container mt-5">
             <div class="card-info">
+                @if($email != 'true')
                 <div class="content-input">
                     <input type="text" placeholder="Ingresa tu tracking" id="tracking">
                     <button class="btn_enviar" onclick="search()">Buscar</button>
                 </div>
+                @endif
                 @if(isset($shipping))
                 <div class="row">
                     <div class="col-md-6">
