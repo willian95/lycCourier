a29785870867637d466eb8ad8fb4b285483c6e9f
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-12-20 13:57:44 -0400

    cambios

--- resources/views/clients/register.blade.php
+++ resources/views/clients/register.blade.php
@@ -3,6 +3,10 @@
 @section('content')
     <div class="login_admin " id="register-dev">
 
+        <div class="loader-cover" v-if="loading == true">
+            <div class="loader"></div>
+        </div>
+
         <div class="row">
             <div class="login100-more mask col-md-6"
                 style="background-image: url('https://images.unsplash.com/photo-1568731053253-f99d388659f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80');">
