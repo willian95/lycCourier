d636d2b54c3c4556bdc210248aa68248c116484f
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-12-22 15:01:44 -0400

    cambios

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
                                                 @if(\Auth::user()->role_id < 3)
                                                 <a title="Editar" :href="'{{ url('/shippings/show') }}'+'/'+shipping.tracking" class="btn btn-info"><i class="far fa-eye"></i></a>
                                                 @else
-                                                <a title="Editar" :href="'{{ url('clients/shipping/') }}'+'/'+shipping.tracking" class="btn btn-info"><i class="far fa-eye"></i></a>
+                                                    <a title="Editar" v-if="shipping.is_finished == 0" :href="'{{ url('clients/shipping/') }}'+'/'+shipping.tracking" class="btn btn-info"><i class="far fa-eye"></i></a>
                                                 @endif
                                                 <a title="Etiqueta" :href="'{{ url('/shippings/qr') }}'+'/'+shipping.id" class="btn btn-info" target="_blank"><i class="far fa-file-pdf"></i></a>
                                                 {{--<button class="btn btn-secondary"><i class="far fa-trash-alt"></i></button>--}}
