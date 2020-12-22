86705f48fff0a530e2ec68adb0321ca47668fa6f
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-12-21 20:47:42 -0400

    cambios

--- app/Http/Controllers/ClientShippingController.php
+++ app/Http/Controllers/ClientShippingController.php
@@ -72,7 +72,7 @@ class ClientShippingController extends Controller
 
             }
 
-            $data = ["messageTitle" => "Nuevo envío creado", "messageMail" => "El cliente ".\Auth::user()->name." ".\Auth::user()->lastname." ha creado un nuevo envío con el tracking: <strong>".$request->tracking."</strong>", "shippingId" => $shipping->id];
+            $data = ["messageTitle" => "Nuevo envío creado", "messageMail" => "El cliente ".\Auth::user()->name." ".\Auth::user()->lastname." ha creado un nuevo envío con el tracking: <strong>".$request->tracking."</strong>", "shippingId" => $shipping->tracking];
 
             foreach(AdminMail::all() as $admin){
 
