4fd2a1b5c9e6cec81d9e8e91276afeb68b3dc004
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2021-01-04 08:09:52 -0400

    cambios

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -53,21 +53,18 @@
                 <div class="modal fade" id="forgotPasswordModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div class="modal-dialog" role="document">
                         <div class="modal-content">
-                            <div class="modal-header">
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <i aria-hidden="true" class="ki ki-close"></i>
-                                </button>
-                            </div>
                             <div class="modal-body">
-                                <div class="form-group">
-                                    <label for="emailRestore">Ingresa tu correo</label>
-                                    <input type="text" class="form-control" id="emailRestore" v-model="emailRestore">
-                                    <small v-if="restoreErrors.hasOwnProperty('email')">@{{ restoreErrors['email'][0] }}</small>
+
+                                <div class="wrap-input100 validate-input">
+                                    <input class="input100" type="text" class="form-control" id="emailRestore" v-model="emailRestore">
+                                    <span class="focus-input100"></span>
+                                    <span class="label-input100">Ingresa tu correo</span>
                                 </div>
+                                <small v-if="restoreErrors.hasOwnProperty('email')">@{{ restoreErrors['email'][0] }}</small>
                                 
                             </div>
                             <div class="modal-footer">
-                                <button type="button" class="btn btn-light-primary font-weight-bold" data-dismiss="modal">Cerrar</button>
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
                                 <button type="button" class="btn btn-primary font-weight-bold"  @click="passwordRestore()">Recuperar</button>
                             </div>
                         </div>
