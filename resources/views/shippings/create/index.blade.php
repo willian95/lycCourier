12e7e25fe38a90ee8d1b808a425c1f4ed1d4f1b7
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2021-02-23 14:51:49 -0400

    cambios

--- resources/views/shippings/create/index.blade.php
+++ resources/views/shippings/create/index.blade.php
@@ -34,7 +34,7 @@
                                 <h3 class="text-center">Detalles del envío</h3>
                             </div>
                             <div class="col-12">
-                                <h3>Warehouse #: {{ DB::select("SHOW TABLE STATUS LIKE 'shippings'") }}</h3>
+                                <h3>Warehouse #: {{ "WRI".str_pad(DB::select("SHOW TABLE STATUS LIKE 'shippings'")[0]->Auto_increment, 7, "0", STR_PAD_LEFT) }}</h3>
                             </div>
                             <div class="col-md-6">
                                 <div class="form-group">
