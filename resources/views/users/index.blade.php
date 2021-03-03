464b1a3d429af4b357de0bb6d5573223cfcb84d3
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2021-03-03 16:25:52 -0400

    cambios

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -94,7 +94,7 @@
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Crear usuario</h5>
+                        <h5 class="modal-title" id="exampleModalLabel">Usuario</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <i aria-hidden="true" class="ki ki-close"></i>
                         </button>
