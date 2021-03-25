7a7cf4a511f3217ca32eb5ab724c6ea1c20eeb74
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2021-03-25 15:08:53 -0400

    cambios

--- app/ShippingGuide.php
+++ app/ShippingGuide.php
@@ -3,9 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ShippingGuide extends Model
 {
+
+    use SoftDeletes;
+
     public function shippings(){
         return $this->hasMany(Shipping::class);
     }
